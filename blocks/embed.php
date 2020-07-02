756a03036cec398f8ffb962ba53e91be7ffeb03c
Jerome Paulos <jeromepaulos@yahoo.com>
2020-07-01 23:18:51 -0700

    Advanced embed block

---
+++ blocks/embed.php
@@ -0,0 +1,3 @@
+<div class="advanced-embed <?php echo get_field('width'); ?>" style="height:<?php echo get_field('height'); ?>px;overflow:hidden;">
+    <iframe src="<?php echo esc_url( get_field('url') ); ?>" style="width:100%;height:100%;border:none;outline:none;" class="advanced-iframe"></iframe>
+</div>
