711c4320c89b21d96ea50bfb5911f54cff9d9bbc
Jerome Paulos <jeromepaulos@yahoo.com>
2020-08-22 17:49:19 -0700

    Update search.php

--- search.php
+++ search.php
@@ -1,49 +1,41 @@
+<?php
+$count = (new WP_Query("s=$s&showposts=0"))->found_posts;
+?>
+
 <?php get_header(); ?>
 <main id="search">
 
-    <form class="search-form" action="<?php echo get_bloginfo( 'url' ); ?>">
-        <input class="search-form-input" type="text" name="s" value="<?php the_search_query(); ?>">
-        <button><i class="far fa-search"></i></button>
-    </form>
+    <div class="search-form">
+        <span class="search-result-count"><?php echo number_format($count); ?> results found for</span>
+        <form action="<?php echo get_bloginfo('url'); ?>">
+            <input type="text" name="s" value="<?php the_search_query(); ?>">
+            <button><i class="far fa-search"></i></button>
+        </form>
+    </div>
 
-    <div class="results" data-page="1" data-query="<?php the_search_query(); ?>">
+    <div class="article-stream">
 
-    <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
-        <?php if( get_post_type() !== 'page' ) { ?>
-
-        <div class="search-result<?php if( !has_post_thumbnail() && get_post_type() == 'post' ) { echo ' no-image'; } ?>">
-
-            <p class="result-date"><?php echo get_the_date( 'F j, Y' ); ?></p>
-
-            <div class="result-article">
-                <a href="<?php echo get_permalink(); ?>" class="result-title"><?php echo get_the_title(); ?></a>
-                <?php if( get_post_type() == 'column' ) { ?>
-                    <p class="result-meta">Column by <?php coauthors_posts_links(); ?></p>
-                <?php } else { ?>
-                <p class="result-meta">By <?php coauthors_posts_links(); ?> in <?php echo get_the_category()[0]->name; ?></p>
-                <?php } ?>
-            </div>
-
-            <?php if( get_post_type() == 'column' ) { ?>
-            <a href="<?php echo get_permalink(); ?>">
-                <img class="columnist-image" src="<?php echo get_avatar_url( get_the_author_meta( 'ID' ) ); ?>">
+    <?php if( have_posts() ) { ?>
+        <?php while( have_posts() ) { the_post(); ?>
+            <a class="stream-item" data-type="<?php echo get_post_type(); ?>" href="<?php echo get_permalink(); ?>">
+                <div class="article-info">
+                    <span class="article-section"><?php echo getSection(); ?></span>
+                    <h2 class="article-title"><?php echo get_the_title(); ?></h2>
+                    <p class="article-excerpt">
+                        <span class="article-date"><?php echo get_the_date(); ?></span>
+                        <?php echo getExcerpt(); ?>
+                    </p>
+                </div>
+                <?php get_post_type() == 'column' ? theColumnImage() : the_post_thumbnail('small-three-two'); ?>
             </a>
-            <?php } else { ?>
-            <a href="<?php echo get_permalink(); ?>">
-                <?php the_post_thumbnail( 'small-three-two' ); ?>
-            </a>
-            <?php } ?>
-
-        </div>
-        
         <?php } ?>
-    <?php endwhile; ?>
-
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/loader.svg" class="search-loader">
-
-    <?php else: ?>
-        <p class="no-results">No results found.</p>
-    <?php endif; ?>
+        <div class="navigation">
+            <div class="previous-page"><?php previous_posts_link( '&laquo; Previous Page' ); ?></div>
+            <div class="next-page"><?php next_posts_link( 'Next Page &raquo;', '' ); ?></div>
+        </div>
+    <?php } else { ?>
+        <p>No results found.</p>
+    <?php } ?>
 
     </div>
 
