e6d499e7f15dad4c855812b7e864c977d403397a
Jerome Paulos <jeromepaulos@yahoo.com>
2020-08-27 20:09:25 -0700

    Rewrite slideshow in ES6 + CSS

--- js/front-page.js
+++ js/front-page.js
@@ -1,10 +1,21 @@
-$(window).resize(function(){ $('.slideshow').css('height', $('.slideshow > img:first-child').height()); })
-$(window).ready(function(){
-    $('.slideshow').css('height', $('.slideshow > img:first-child').height());
-    
-    $('.slideshow > img:not(:first-child)').hide();
-    setInterval(function() {
-        $('.slideshow > img').stop(true, true);
-        $('.slideshow > img:first-child').fadeOut().next('img').fadeIn().end().appendTo('.slideshow');
-    }, 2500);
-})
+var currentSlide = 0,
+    slideshow = document.querySelector('.slideshow'),
+    slides = slideshow.querySelectorAll('img');
+
+setInterval( () => {
+
+    slides.forEach( img => {
+        img.style.opacity = 0;
+    } )
+
+    currentSlide = (currentSlide != slides.length - 1) ? currentSlide + 1 : 0;
+    slides[currentSlide].style.opacity = 1;
+
+}, 3000 )
+
+function setSlideshowSize() {
+    slideshow.style.height = getComputedStyle( slides[currentSlide] ).height;
+}
+
+window.addEventListener( 'resize', setSlideshowSize );
+window.addEventListener( 'load', setSlideshowSize );
