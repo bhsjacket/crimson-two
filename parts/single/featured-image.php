21669d0491c01b1080e65f00fe8f51acf2952135
Jerome Paulos <jeromepaulos@yahoo.com>
2020-09-18 22:02:29 -0700

    Support new vertical image layout

--- parts/single/featured-image.php
+++ parts/single/featured-image.php
@@ -1,4 +1,14 @@
-<div class="featured-image-outer <?php echo get_field('image_size') ?? 'align-normal'; ?>">
+<?php
+
+if( get_field('image_size') ?? 'align-normal' == 'align-normal' ) { // It sounds stupid but trust me you need it
+    $sizes = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+    $width = $sizes[1];
+    $height = $sizes[2];
+}
+
+?>
+
+<div class="featured-image-outer <?php echo get_field('image_size') ?? 'align-normal'; echo $width / $height <= 0.8 ? ' vertical-image' : null; ?>">
     <?php the_post_thumbnail('large', [
         'class' => 'featured-image zoom'
     ]) ?>
