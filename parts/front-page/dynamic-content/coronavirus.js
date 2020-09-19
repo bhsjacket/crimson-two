8eb00b7b899ff5cde496608f8de8bff36ddbcbe9
Jerome Paulos <jeromepaulos@yahoo.com>
2020-09-18 22:03:33 -0700

    AJAX load coronavirus widget

---
+++ parts/front-page/dynamic-content/coronavirus.js
@@ -0,0 +1,36 @@
+$(document).ready( () => {
+
+    function setData(data) {
+        $('[data-stat="berkeley-total"]').text( data.berkeley.total.toLocaleString() );
+        $('[data-stat="berkeley-new"]').text( data.berkeley.new.toLocaleString() + ' new' );
+        $('[data-stat="alameda-total"]').text( data.alameda.total.toLocaleString() );
+        $('[data-stat="alameda-new"]').text( data.alameda.new.toLocaleString() + ' new' );
+        $('[data-stat="date"]').text( data.date );
+        sparkline.sparkline( $('.berkeley-sparkline')[0], data.berkeley.cumulative );
+
+        var expirationDate = new Date();
+        expirationDate.setDate(new Date().getDate() + 1);
+        expirationDate.setHours(0,0,0,0);
+
+        if( !Cookies.get('coronavirus-cache') ) {
+            Cookies.set( 'coronavirus-cache', JSON.stringify(data), {expires: expirationDate} );
+        }
+
+        $('.coronavirus-stat-dc').removeClass('loading');
+    }
+
+    if( Cookies.get('coronavirus-cache') ) {
+        var data = Cookies.get('coronavirus-cache');
+        data = JSON.parse(data);
+        setData(data);
+    } else {
+        $.ajax({
+            method: 'GET',
+            url: 'https://bhsjacket.local/coronavirus/coronavirus-data/api.php',
+            success: data => {
+                setData(data);
+            }
+        })
+    }
+
+} )
--- parts/front-page/dynamic-content/coronavirus.php
+++ parts/front-page/dynamic-content/coronavirus.php
@@ -1,33 +1,27 @@
-<?php
-$data = file_get_contents('https://jeromepaulos.com/bhsjacket/coronavirus/data.php?data=berkeley');
-$data = json_decode($data, true);
-
-foreach($data as $stat) {
-    $labels[] = 'label';
-    $cases[] = $stat['bklhj_cumulcases'];
-    $new_cases[] = $stat['bklhj_newcases'];
-}
-$new_cases = array_reverse($new_cases);
-$new_cases = $new_cases[0];
-$total_cases = array_reverse($cases);
-$total_cases = $total_cases[0];
-$cases = implode(',', $cases);
-$labels = implode("','", $labels);
-
-$ac_data = file_get_contents('https://jeromepaulos.com/bhsjacket/coronavirus/data.php?data=alameda');
-$ac_data = json_decode($ac_data, true);
-@$ac_data = array_reverse($ac_data);
-$ac_data = $ac_data[0]['attributes'];
-?>
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://bhsjacket.local/wp-content/themes/crimson/js/js.cookie.min.js"></script>
+<link rel="stylesheet" href="https://bhsjacket.local/wp-content/themes/crimson/style.css">
+<link rel="stylesheet" href="https://bhsjacket.local/wp-content/themes/crimson/css/variables.css">
+<link rel="stylesheet" href="https://use.typekit.net/erm4tbu.css">
+<style>
+    .coronavirus-stat-dc {
+        width: 275px;
+        height: 409.09px!important;
+        box-sizing: border-box;
+    }
+</style> -->
+<?php // require_once( '/Users/jerome/Local Sites/berkeley-high-jacket/app/public/wp-load.php' ); ?>
 
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/parts/front-page/dynamic-content/coronavirus.css">
+<script src="<?php echo get_template_directory_uri(); ?>/js/sparkline.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/parts/front-page/dynamic-content/coronavirus.js"></script>
 
-<a href="https://berkeleyhighjacket.com/2020/multimedia/live-coronavirus-data/" class="coronavirus-stat-dc">
+<a class="coronavirus-stat-dc loading" href="https://berkeleyhighjacket.com/2020/multimedia/live-coronavirus-data/">
 
     <h2 class="section-header">Covid-19</h2>
     <div class="chart">
         <svg class="berkeley-sparkline" width="245" height="93" stroke-width="3"></svg>
-        <span class="chart-stat"><?php echo $total_cases; ?></span>
+        <span class="chart-stat" data-stat="date">???</span>
         <div class="chart-dot"></div>
     </div>
 
@@ -35,11 +29,11 @@ $ac_data = $ac_data[0]['attributes'];
         <h3 class="coronavirus-title">Berkeley</h3>
         <div class="coronavirus-stats">
             <div class="coronavirus-stat">
-                <span class="stat-number"><?php echo $total_cases; ?></span>
+                <span class="stat-number" data-stat="berkeley-total">???</span>
                 <span class="stat-label">total cases</span>
             </div>
             <div class="coronavirus-stat">
-                <span class="stat-number"><?php echo $new_cases; ?> new</span>
+                <span class="stat-number" data-stat="berkeley-new">??? new</span>
                 <span class="stat-label">since yesterday</span>
             </div>
         </div>
@@ -51,22 +45,14 @@ $ac_data = $ac_data[0]['attributes'];
         <h3 class="coronavirus-title">Alameda County</h3>
         <div class="coronavirus-stats">
             <div class="coronavirus-stat">
-                <span class="stat-number"><?php echo number_format($ac_data['AC_CumulCases']); ?></span>
+                <span class="stat-number" data-stat="alameda-total">???</span>
                 <span class="stat-label">total cases</span>
             </div>
             <div class="coronavirus-stat">
-                <span class="stat-number"><?php echo number_format($ac_data['AC_Cases']); ?> new</span>
+                <span class="stat-number" data-stat="alameda-new">??? new</span>
                 <span class="stat-label">since yesterday</span>
             </div>
         </div>
     </div>
 
 </a>
-
-<script src="<?php echo get_template_directory_uri(); ?>/js/sparkline.js"></script>
-
-<script>
-
-sparkline.sparkline(document.querySelector('.berkeley-sparkline'), [<?php echo $cases; ?>]);
-
-</script>
