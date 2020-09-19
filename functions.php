29340295f33762c1009ebb34e080d73d7d894dfd
Jerome Paulos <jeromepaulos@yahoo.com>
2020-09-18 22:04:22 -0700

    Move acf.php below plugin include

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ require_once('functions/head.php'); // Cleanup Auto-Generated <head> Tag
 require_once('functions/editors.php'); // Customize Editors
 require_once('functions/register.php'); // Register Taxonomies & Custom Post Types
 require_once('functions/misc.php'); // Miscellaneous Functions
-require_once('functions/acf.php'); // Register ACF Fields
 
 // ADVANCED CUSTOM FIELDS
 define( 'ACF_PATH', get_stylesheet_directory() . '/includes/acf/' );
@@ -23,3 +22,5 @@ add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
 function my_acf_settings_show_admin( $show_admin ) {
     return false;
 } */
+
+require_once('functions/acf.php'); // Register ACF Fields
