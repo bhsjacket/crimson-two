3fa532bf81564dd5ccb1300e41896afab812894a
Jerome Paulos <jeromepaulos@yahoo.com>
2020-09-18 21:58:35 -0700

    Remove styles on class change

--- js/lazy-load-fix.js
+++ js/lazy-load-fix.js
@@ -1,10 +1,30 @@
-(function() {
-    var images = document.querySelectorAll('img[width][height]');
-    images.forEach(image => {
-        var width = parseInt( getComputedStyle(image).width );
-        var aspectRatio = parseInt( image.getAttribute('width') ) / parseInt( image.getAttribute('height') );
+(()=>{
+
+var images = document.querySelectorAll('img[width][height]');
+images.forEach(image => {
+
+    var width = parseInt( getComputedStyle(image).width );
+    var aspectRatio = parseInt( image.getAttribute('width') ) / parseInt( image.getAttribute('height') );
+
+    image.dataset.width = image.style.width;
+    image.dataset.height = image.style.height;
+
+    image.style.width = width + 'px';
+    image.style.height = width / aspectRatio + 'px';
+
+    var observer = new MutationObserver( () => {
+        if( image.classList.contains('lazy-loaded') ) {
+            image.style.width = image.dataset.width;
+            image.style.height = image.dataset.height;
+        }
+    });
+    observer.observe(image, {
+        attributes: true, 
+        attributeFilter: ['class'],
+        childList: false,
+        characterData: false
+    });
     
-        image.style.width = width + 'px';
-        image.style.height = width / aspectRatio + 'px';
-    })
+})
+
 })()
