db5bc15b5fe65da5169309e98d19e991b639156a
Jerome Paulos <jeromepaulos@yahoo.com>
2020-10-03 10:18:37 -0700

    Newsletter popup

--- parts/newsletter.php
+++ parts/newsletter.php
@@ -1,5 +1,9 @@
-<?php if( $_COOKIE['newsletter-popup'] !== 'true' ) {
-    setcookie( 'newsletter-popup', 'true', strtotime('+2 days') ); ?>
+<?php
+
+if( $_COOKIE['newsletter-popup'] !== 'true' || $_COOKIE['newsletter-popup'] !== true ) {
+    setcookie( 'newsletter-popup', 'true', strtotime('+2 days') );
+    
+?>
 
 <div class="popup hidden">
     <div class="email-popup">
@@ -9,6 +13,7 @@
         <p class="email-description">Our biweekly newsletter includes a curated selection of the best student journalism produced by the <em>Jacket</em>.</p>
         <form class="email-form" target="_blank" action="https://bhsjacket.us20.list-manage.com/subscribe/post?u=cd770a9475cd688fc6dcac8f1&id=43ce4620e7" method="POST">
             <input type="email" name="EMAIL" placeholder="Enter email address">
+            <input type="hidden" name="SOURCE" value="Website Popup">
             <!-- Spam Protection -->
             <div style="position: absolute; left: -5000px;" aria-hidden="true">
                 <input type="text" name="b_cd770a9475cd688fc6dcac8f1_43ce4620e7" tabindex="-1" value="">
@@ -25,17 +30,27 @@
         popupBody = popup.querySelector('.email-popup'),
         animatedIcon = document.getElementById('animated-newsletter-icon');
 
-    setTimeout( () => {
-        popup.classList.remove('hidden');
-    }, 3000)
+    if( Cookies.get('newsletter-popup') ) {
+        
+        popup.remove();
 
-    setTimeout( () => {
-        animatedIcon.classList.remove('not-animated');
-    }, 4000 )
+    } else {
 
-    popup.querySelector('.close-popup').addEventListener('click', () => {
-        popup.classList.add('hidden');
-    })
+        setTimeout( () => {
+            popup.classList.remove('hidden');
+        }, 3000)
+    
+        setTimeout( () => {
+            animatedIcon.classList.remove('not-animated');
+        }, 4000 )
+    
+        popup.querySelector('.close-popup').addEventListener('click', () => {
+            popup.classList.add('hidden');
+        })
+    
+        Cookies.set('newsletter-popup', 'true', { expires: 3 })
+
+    }
 
     })();
 </script>
