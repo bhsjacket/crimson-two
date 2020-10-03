dfdfc37fa3386e85e499dbb7c647424c5f227f0c
Jerome Paulos <jeromepaulos@yahoo.com>
2020-10-03 10:18:10 -0700

    Audio player block

--- functions/acf.php
+++ functions/acf.php
@@ -104,3 +104,20 @@ acf_register_block_type([
         'multiple' => true,
     ],
 ]);
+
+acf_register_block_type([
+    'name' => 'audio',
+    'title' => 'Audio',
+    'category' => 'embed',
+    'keywords' => [ 'audio', 'sound', 'embed', 'upload' ],
+    'mode' => 'edit',
+    'render_template' => 'blocks/audio.php',
+    'enqueue_style' => get_template_directory_uri() . '/blocks/audio-block.css',
+    'enqueue_script' => get_template_directory_uri() . '/blocks/audio.js',
+    'icon' => '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>',
+    'supports' => [
+        'align' => [ 'center' ],
+        'mode' => false,
+        'multiple' => true,
+    ],
+]);
